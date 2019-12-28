cb84a1fd9ae41374856ea227c1f4f8ffa682292a
Marcin Kozak <marcinkozak005@gmail.com>
2019-12-28 19:45:57 +0100

    Finished sortings in Lab4

--- Lab4/lab4.erl
+++ Lab4/lab4.erl
@@ -1,5 +1,5 @@
 % hello world program
--module(lab4).
+-module(helloworld).
 %-export([start/0]).
 %-export([pole/1,objetosc/1]).
 
@@ -82,8 +82,25 @@ merge([Ha|Ta],[Hb|Tb]) ->
 mergeSort([]) -> [];
 mergeSort([A]) -> [A];
 mergeSort(List) ->
-		{Left,Right} = lists:split(trunc(length(List)/2),List),
-		merge(mergeSort(Left),mergeSort(Right)).
+	{Left,Right} = lists:split(trunc(length(List)/2),List),
+	merge(mergeSort(Left),mergeSort(Right)).
+		
+
+bubbleSort([]) -> [];
+bubbleSort([A]) -> [A];
+bubbleSort(List) ->
+    LastSorted = helperBubbleSort(List),
+    bubbleSort(lists:sublist(LastSorted,length(LastSorted)-1)) ++ [lists:last(LastSorted)].
+        
+helperBubbleSort([]) -> [];
+helperBubbleSort([X]) -> [X];
+helperBubbleSort([A,B|T]) ->
+    if
+        A > B -> [B|helperBubbleSort([A|T])];
+        true -> [A|helperBubbleSort([B|T])]
+    end.
+
+
 
 
 
@@ -106,13 +123,13 @@ objetosc({stozek,R,H}) -> pi()*R*R*H/3.
 polaFigur([H]) -> [pole(H)];
 polaFigur([H|T]) -> [pole(H)|polaFigur(T)].
 
+
 start() ->
     io:fwrite("Pole trojkata: ~w\n",[pole({trojkat,3,4,5})]),
     io:fwrite("Lista ~w\n",[[1,2,34,123,20,11]]),
     io:fwrite("Len listy ~w : ~w\n",[[1,2,34,123,20,11],len([1,2,34,123,20,11])]),
     io:fwrite("Max i min listy (lista): ~w\n",[lMinMax([1,2,34,123,20,11])]),
     io:fwrite("Max i min listy (krotka): ~w\n",[kMinMax([1,2,34,123,20,11])]),
-    io:fwrite("Generowana N..1: ~w",[generujListe(8)]),
-    io:fwrite("Lista pol: ~w",[polaFigur([{kolo,2},{kula,12},{kwadrat,5,5},{stozek,12,5}])]).
-    
-    
+    io:fwrite("Generowana N..1: ~w\n",[generujListe(8)]),
+    io:fwrite("Lista pol: ~w\n",[polaFigur([{kolo,2},{kula,12},{kwadrat,5,5},{stozek,12,5}])]),
+    io:fwrite("Lista BubbleSorted: ~w",[bubbleSort([1,2,34,123,20,11])]).
